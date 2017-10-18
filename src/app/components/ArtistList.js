68213130cf1a2f542c3f3fe34616b68631c004c1
ghostanza <friedcaligari@gmail.com>
2017-10-18 16:10:43 -0400

    update TopArtists styles for dropdown, add ArtistList component

---
+++ src/app/components/ArtistList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+export default class ArtistList extends React.Component {
+  constructor(p){
+    super(p);
+    this.triggerSelection = this.triggerSelection.bind(this);
+  }
+  triggerSelection(e){
+    let id = e.currentTarget.dataset.id;
+    this.props.toggleSelected(id);
+  }
+  render() {
+    return(
+      <ul>
+        {this.props.artists.map((i) => {
+          return (
+            <li key={i.id}
+              data-id={i.id}
+              onClick={this.triggerSelection}
+              className={this.props.selected.indexOf(i.id) >= 0 ? "selected" : ""}>
+              {i.images && i.images.length > 2 ? (<div className='artist-img' style={{'backgroundImage': `url(${i.images[2].url})`}}></div>) : ''}
+              <span className='artist-name'>{i.name}</span>
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
--- src/app/components/TopArtists.js
+++ src/app/components/TopArtists.js
@@ -1,50 +1,55 @@
 import React from 'react';
+import ArtistList from './ArtistList';
 
 export default class TopArtists extends React.Component {
   constructor(p){
     super(p);
     this.state = {
       topTime: 'short_term',
-      timeOptions: [{type: 'short_term', label: 'Recent'}, {type: 'medium_term', label: '6 Months'},{type: 'long_term', label: 'All Time'} ]
+      timeOptions: [
+        {type: 'short_term', label: 'Recent'},
+        {type: 'medium_term', label: '6 Months'},
+        {type: 'long_term', label: 'All Time'}
+      ]
     }
     this.handleTimeChange = this.handleTimeChange.bind(this);
     this.toggleSelected = this.toggleSelected.bind(this);
   }
   handleTimeChange(e){
-    let val = e.target.value;
+    let val = e.target.dataset.range;
     this.setState((p) => {
       return { ...p, topTime: val}
     });
   }
-  toggleSelected(e){
-    let id = e.currentTarget.dataset.id;
+  toggleSelected(id){
     this.props.updateSelected(id);
   }
+  toggleOpen(e){
+    e.target.classList.toggle('closed');
+  }
   render() {
-    console.log(this.state);
     return(
-      <div className="top-artist-wrapper">
+      <div className="top-artist-wrapper tab">
       {this.props.artists && Object.keys(this.props.artists).length > 0 ? (
         <div className="top-artists">
-          <h3>My Top Artists</h3>
-            <select value={this.state.topTime} onChange={this.handleTimeChange}>
+          <span className="tab-label closed" onClick={this.toggleOpen}>My Top Artists</span>
+          <div className="top-artists-list">
+            <div className="time-range-container">
               {this.state.timeOptions.map((i)=>{
-                return <option value={i.type} key={i.type}>{i.label}</option>
-              })}
-            </select>
-            <ul>
-              {this.props.artists[this.state.topTime].map((i) => {
                 return (
-                  <li key={`${this.state.topTime}${i.id}`}
-                    data-id={i.id}
-                    onClick={this.toggleSelected}
-                    className={this.props.selected.indexOf(i.id) >= 0 ? "selected" : ""}>
-                    {i.images && i.images.length > 2 ? (<div className='artist-img' style={{'backgroundImage': `url(${i.images[2].url})`}}></div>) : ''}
-                    <span className='artist-name'>{i.name}</span>
-                  </li>
+                  <span
+                    className={`time-range ${i.type === this.state.topTime ? 'active' : ''}`}
+                    data-range={i.type}
+                    key={i.type}
+                    onClick={this.handleTimeChange}>{i.label}</span>
                 )
               })}
-            </ul>
+            </div>
+            <ArtistList
+              artists={this.props.artists[this.state.topTime]}
+              selected={this.props.selected}
+              toggleSelected={this.toggleSelected}/>
+          </div>
         </div>
         ) : ''}
         </div>
